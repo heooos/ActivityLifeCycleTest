c63138766095e532c3a2578816ca436403f30f10
heooos <hao1993624@gmail.com>
2015-05-17 00:04:42 +0800

    改了无关紧要的东西  还是无法启动

--- src/com/hao/activitylifecycletest/DialogActivity.java
+++ src/com/hao/activitylifecycletest/DialogActivity.java
@@ -1,16 +1,14 @@
 package com.hao.activitylifecycletest;
 
-import com.hao.activitylifecycletest.R;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
 
 public class DialogActivity extends Activity {
-		@Override
-		protected void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			requestWindowFeature(Window.FEATURE_NO_TITLE);
-			setContentView(R.layout.dialog_layout);
-		}
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.dialog_layout);
+	}
 }
